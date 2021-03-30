066f5a10b9a9724a2994d8d40ea360bb2b80e420
jesseandringa <jessandringa@gmail.com>
2021-03-30 11:06:27 -0600

    buttons working

--- Code/chrome_ext_frame/popup.js
+++ Code/chrome_ext_frame/popup.js
@@ -1,11 +1,78 @@
-document.addEventListener('DOMContentLoaded',function(){
-  document.querySelector('button').addEventListener('click',onclick,false)
+// document.addEventListener('DOMContentLoaded',function(){
+//   document.querySelector('button').addEventListener('click',onclick,false)
 
-  function onclick () {
-    chrome.tabs.query({currentWindow: true, active: true},
-    fuction(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, 'hi')
-    })
+//   function onclick () {
+//     chrome.tabs.query({currentWindow: true, active: true},
+//     fuction(tabs) {
+//       chrome.tabs.sendMessage(tabs[0].id, 'hi')
+//     })
+//   }
+// },false)
+
+//function to link nav bar to home page website
+document.addEventListener('DOMContentLoaded', function () {
+  var links = document.getElementsByTagName("a");
+  for (var i = 0; i < links.length; i++) {
+      (function () {
+          var ln = links[i];
+          var location = ln.href;
+          ln.onclick = function () {
+              chrome.tabs.create({active: true, url: location});
+          };
+      })();
   }
-},false)
+});
+
+//function to link the paypal button to 
+document.addEventListener('DOMContentLoaded', function () {
+  var links = document.getElementsByTagName("form");
+  for (var i = 0; i < links.length; i++) {
+      (function () {
+          var ln = links[i];
+          var location = "https://www.paypal.com/donate?token=KXeAv_tPqxWHXJLecDPFIgAA2u2mfMVlN9NMTxPF8PiaNXC-TU9wjbkpO5Or5blDKBD6ZD8uAnsdqIt4";
+          ln.onclick = function () {
+              chrome.tabs.create({active: true, url: location});
+          };
+      })();
+  }
+});
+
+//function "yes" button to hide yes no buttons
+  // also will display paypal button 
+  document.getElementById("yesBtn").addEventListener("click", function(){
+    document.getElementById("button_div").style.display="none";
+    document.getElementById("paypal_div").style.display="initial";
+    document.getElementById("footer").style.left="37%";
+    document.getElementById("footer").style.position="fixed";
+  });
+
+  //function "no" button hides buttons displays message
+  document.getElementById("noBtn").addEventListener("click", function(){
+    document.getElementById("button_div").style.display="none";
+    document.getElementById("no_selected").style.display="initial";
+    document.getElementById("footer").style.left="37%";
+    document.getElementById("footer").style.position="fixed";
+  });
+ 
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+
+document.getElementById("mySidebar").addEventListener("click",function w3_open() {
+var mySidebar = document.getElementById("mySidebar");
+
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+});
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
+//calculate cost 
+//paypal button
+document.getElementById("cost").addEventListener("onLoad", function costCalc(){
+  document.getElementById("cost").value = 99;
+});
 
