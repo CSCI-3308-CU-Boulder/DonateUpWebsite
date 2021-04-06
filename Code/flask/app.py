38418c12f1eee53c178e45b19f6c03d513dc595f
thursday <ashour.timothy@gmail.com>
2021-04-05 21:26:46 -0600

    basic psql login and session working

--- Code/flask/app.py
+++ Code/flask/app.py
@@ -2,23 +2,24 @@
 from flask import Flask, render_template, current_app, flash, redirect, request, session, abort
 import os
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import sessionmaker
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://psql:psql@localhost:5432/donateup_test"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://dbuser:pass@localhost:5432/donateup"
 db = SQLAlchemy(app)
 
-#class userModel(db.Model):
-#    __tablename__ = 'users'
+class userModel(db.Model):
+    __tablename__ = 'users'
 #
-#    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
 #    name = db.Column(db.String())
-#    email = db.Column(db.String())
-#    password = db.Column(db.String())
+    email = db.Column(db.String())
+    password = db.Column(db.String())
 #    
-#    def __init__(self, name, email, password):
+    def __init__(self, name, email, password):
 #        self.name = name
-#        self.email = email
-#        self.password = password
+        self.email = email
+        self.password = password
 #
 #    def __repr__(self):
 
@@ -31,10 +32,32 @@ def render_static(page_name):
 def home():
     return render_template('home.html')
 
+@app.route('/account')
+def account():
+    if session['logged_in'] == True:
+        return render_template('pay.html')
+    else:
+        return home()
+
 # Made with help from https://pythonspot.com/login-authentication-with-flask/
 
 @app.route('/login', methods=['POST'])
 def user_login():
+    Session = sessionmaker(bind=db)
+    s = Session()
+    userLoginRequest = request.form['email']
+    passwordLoginRequest = request.form['pwd']
+    userLoginRequest = userModel.query.filter_by(email=request.form['email']).first()
+    if userLoginRequest:
+        session['logged_in'] = True
+        return account()
+    else:
+        flash('wrong password!')
+        return home()
+
+@app.route('/signup', methods=['POST'])
+def user_signup():
+    newUser = userModel(request.form['email'],request.form['password'])
     if request.form['pwd'] == 'pass' and request.form['email'] == 'user':
         session['logged_in'] = True
     else:
